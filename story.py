5494d38aae89776e0501f4a8cade08738de92c6b
dasa777777 <75094391+dasa777777@users.noreply.github.com>
2021-08-28 14:06:20 +0200

    Add files via upload

---
+++ story.py
@@ -0,0 +1,74 @@
+print("Ides z prechadzky. Vystupis z autobusu a ides do podchodu. Mozes vyjst schodami alebo rampou. Co si vyberies?")
+x = input("rampu = r, schody = s: ")
+if x == "r":
+    print("vybral si si rampu a skoro na teba skocil potkan")
+elif x == "s":
+    print("vybral si si schody a potkol si sa o prazdnu flasu od borovicky")
+else:
+    print("nespravny kod")
+
+
+print("prides k Tescu a pred nim posedavaju socky, medzi nimi tvoj kamarat Marek, ktory sa prechlastal k invalidite. Marek na teba mava a vola: Ahoj! Co spravis?")
+y = input("odzdravim = y, ignorujem = n: ")
+if y == "y":
+    print("Marek sa potesi a porozprava ti, kolko toho vypil.")
+elif y == "n":
+    print("Marek sa urazi a zacne po tebe vykrikovat, ze si fasista.")
+else:
+    print("netrafil si pismeno")
+
+
+print("Ides dalej po chodniku a prides na razcestie. Mozes ist priechodom alebo dalej po chodniku. Co si vyberies?")
+z = input("priechod= p, chodnik = c: ")
+if z == "p":
+    print("V priechode je silny prievan a tak sa vratis naspat.")
+elif z == "c":
+    print("Na chodniku sa venci susedov pes a kruti chvostom.")
+else:
+    print("Skus to este raz")
+
+
+print("Ides dalej po chodniku az prides ku brane. Stoji tam tvoj kamos Mato a umyva okna, ako vzdy. Pozdravis ho a: ")
+x = input("opytas sa ho ako sa ma = y, pojdes dalej = n: ")
+if x == "y":
+    print ("Mato ti porozprava, co ma nove a ako ho rozculuju bordelari.")
+elif x == "n":
+    print("Mato ta poprosi, aby zamykal dvere.")
+else:
+    print("one more time please")
+
+print("Vojdes do brany a stretnes pekne usmievave dievca. Pekne pozdravis a podrzis jej dvere. Co spravis?")
+i = input("opytas sa jej ako sa vola = y, poprajes jej pekny den = n: ")
+if i == "y":
+    print("Dievca ti milo povie, ze do toho ta nic a s chichotom odide.")
+elif i == "n":
+    print("Dievca ti popraje pekny den tiez a doda, ze by bolo fajn zbehnut na pivo.")
+else:
+    print("Game over")
+
+print("Vojdes do bytu a zistis, ze pes ti roztrhal vankuse a vsade poletuje perie. Co spravis?")
+u = input("zderies psa = p, povzdychnes si= n: ")
+if u == "p":
+    print("Pes zaskuci a zalezie pod stol.")
+elif u == "n":
+    print("Pes sa zasmeje a rozviri perie tak, ze sa uporne rozkasles.")
+else:
+    print("nabuduce triafaj presnejsie")
+
+print("Urobis si kavu, sadnes si a zapnes TV. V tv idu spravy a hlasia, ze sa blizi burka. Vonku zacina povievat silny vietor. Co spravis?")
+b = input("Vstanes a pozatvaras okna = a, nic, nechce sa mi vstavat z kresla = b, zivnem a zaspim = c: ")
+if b== "a":
+    print("Na okne pristane cajka a zacne skriekat")
+elif b == "b":
+    print("Silny vietor sfukne crepnik z parapety, crepnik spadne dolu na dvor a polame susedkine magnolie")
+elif b == "c":
+    print("zobudi ta silny rachot")
+else:
+    print("tak este raz a poriadne")
+    
+
+
+
+
+
+    
